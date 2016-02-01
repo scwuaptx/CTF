1a51a88253fdb696692533d1faed048f34d333b4
Angelboy <scwuaptx@gmail.com>
2016-02-01 08:02:17 +0000

    add find stack

--- tools/pwngdb.py
+++ tools/pwngdb.py
@@ -73,7 +73,7 @@ def findstr(pat):
     mems = infomap.split('\n')
     for mem in mems[:-1] :
         start = int((mem.split()[0]).split("-")[0],16)
-        end = int((mem.split()[0]).split("-")[1],16)  
+        end = int((mem.split()[0]).split("-")[1],16) - 0x1
         gdbcmd = "find " + hex(start) + "," + hex(end) + "," + "\"" + pat + "\""
         result = gdb.execute(gdbcmd ,to_string=True)
         if re.search("Pattern not found",result):
@@ -81,9 +81,12 @@ def findstr(pat):
         else :
             for addr in result.split('\n')[:-2]:
                 name = mem.split()[5]
-                content = (gdb.execute("x/s " + addr,to_string=True)).split()[1]
-                output = "\033[34m" + addr + "\033[37m"+ " --> " + content + "\033[32m" + " (" + name + ")"+"\033[37m" + '\n'
-                print(output,end="")
+                try :
+                    content = (gdb.execute("x/s " + addr,to_string=True)).split()[1]
+                    output = "\033[34m" + addr + "\033[37m"+ " --> " + content + "\033[32m" + " (" + name + ")"+"\033[37m" + '\n'
+                    print(output,end="")
+                except :
+                    continue
 
 def libcbase():
     infomap = procmap()
